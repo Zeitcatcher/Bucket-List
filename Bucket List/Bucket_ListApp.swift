b1ccd4c8948ed40f3ba7f66cee9ee0de62e4d877
Zeitcatcher <ars.oksenoyt@gmail.com>
2024-08-27 17:24:11 -1000

    Initial Commit

---
+++ Bucket List/Bucket_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_ListApp.swift
+//  Bucket List
+//
+//  Created by Arseniy Oksenoyt on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
